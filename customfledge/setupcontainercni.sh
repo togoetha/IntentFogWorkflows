c372a023d2e47800ae7efc9106bf1eccf1753db2
togoetha <togoetha.goethals@ugent.be>
2022-06-09 15:01:37 +0200

    qsdf

--- customfledge/setupcontainercni.sh
+++ customfledge/setupcontainercni.sh
@@ -32,7 +32,7 @@ hostif="veth$rand"
 ip link add $cniif type veth peer name $hostif 
 
 #tc qdisc add dev $cniif root tbf rate $bandwidth burst 250000 latency 1ms 
-tc qdisc add dev $cniif root netem rate $bandwidth #delay $latency 
+#tc qdisc add dev $cniif root netem rate $bandwidth #delay $latency 
 
 #link $hostif to cni0
 ip link set $hostif up 
