3a8c26f48cd548d2080e79a3254bdb2fc4fa63b3
togoetha <togoetha.goethals@ugent.be>
2022-06-28 11:52:56 +0200

    2n hs badmm

--- converter/main.go
+++ converter/main.go
@@ -26,7 +26,7 @@ func main() {
 	//fmt.Printf("Loading config file %s\n", cfgFile)
 	config.LoadConfig(cfgFile)
 
-	inputYamlFile := "9ntier12.yml"
+	inputYamlFile := "2n_hs_badmm.yml"
 	if len(argsWithoutProg) > 0 {
 		inputYamlFile = argsWithoutProg[0]
 	}
