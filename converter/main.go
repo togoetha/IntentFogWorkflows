57cc58c885fe2608440f1a2397b2f2b53f556751
togoetha <togoetha.goethals@ugent.be>
2022-06-28 13:39:21 +0200

    n2 hs mincb

--- converter/main.go
+++ converter/main.go
@@ -26,7 +26,7 @@ func main() {
 	//fmt.Printf("Loading config file %s\n", cfgFile)
 	config.LoadConfig(cfgFile)
 
-	inputYamlFile := "2n_hs_badmm.yml"
+	inputYamlFile := "2n_hs_mincb.yml"
 	if len(argsWithoutProg) > 0 {
 		inputYamlFile = argsWithoutProg[0]
 	}
